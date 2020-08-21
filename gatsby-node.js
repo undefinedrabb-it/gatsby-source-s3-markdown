38429671c84cd88fc4fe0cb9d3c8bdeb86c25112
Jakub Zając <jakub.zajac.jan@gmail.com>
2020-08-21 22:53:20 +0200

    init

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,62 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-// You can delete this file if you're not using it
+const NODE_TYPE = "S3Markdown"
 
-/**
- * You can uncomment the following line to verify that
- * your plugin is being loaded in your site.
- *
- * See: https://www.gatsbyjs.org/docs/creating-a-local-plugin/#developing-a-local-plugin-that-is-outside-your-project
- */
-exports.onPreInit = () => console.log("Loaded gatsby-starter-plugin")
+const AWS = require('aws-sdk');
+const axios = require('axios');
+const matter = require('gray-matter');
+
+
+exports.sourceNodes = async ({ actions, createContentDigest, createNodeId }, options) => {
+
+    const bucketName = options.bucket;
+    const region = options.region;
+
+    AWS.config.update(options.aws_credentials);
+
+    s3 = new AWS.S3({ apiVersion: '2006-03-01' });
+
+
+    const { createNode } = actions;
+
+    const data = {
+        S3Markdowns: await getDataFromS3(bucketName, region)
+    };
+
+    data.S3Markdowns.forEach(S3Markdown =>
+        createNode({
+            ...S3Markdown,
+            id: createNodeId(`${NODE_TYPE}-${S3Markdown.data.id}`),
+            // parent: null,
+            // children: [],
+            internal: {
+                type: NODE_TYPE,
+                // mediaType: "text/markdown",
+                contentDigest: createContentDigest(S3Markdown)
+            },
+        })
+    )
+
+    return
+}
+
+
+const getDataFromS3 = async (bucketName, region) => {
+
+    var fileInBucket = await s3.listObjects({
+        Bucket: bucketName,
+    }).promise();
+
+    const fileNames = fileInBucket.Contents.map(element => {
+        return element.Key;
+    });
+
+
+    const markdownTable = await Promise.all(
+        fileNames.map(async (file) => {
+            var response = await axios.get(`https://${bucketName}.s3-${region}.amazonaws.com/${file}`);
+
+            var model = matter(response.data);
+            return model;
+        }));
+
+    return markdownTable;
+}
